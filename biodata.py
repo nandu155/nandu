62f5413cb02ed0998470a8e6430cf3e438bb2f7e
nandu155 <nanduvemula33@gmail.com>
2025-04-16 10:40:37 +0530

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,10 @@
+#Define a function 'personal_details'.
+def personal_details():
+    #Define variables 'name' and 'age' and assign values to them.
+    name, age = "nandu","19"
+    #Define a variable 'address'and assign a value to it.
+    address = "Nizampet,Telangana,India"
+    #print the personal details using string formatting.
+    print("Name:{}\nAge:{}\nAddress:{}".format(name,age,address))
+#Call the 'personal_details'function to display the details.
+personal_details()
